89f4c5f906a16b2b81966bafa40ac4b46e3b7ece
Andrey Podolyako <andreypodolyako@gmail.com>
2021-03-07 13:34:03 +0300

    Decompose reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-    return (
+	const dispatch = useDispatch();
+	const cash = useSelector(state => state.cash)
+
+	const addCash = (cash) => {
+		dispatch({type: "ADD_CASH", payload: cash})
+	}
+
+	const getCash = (cash) => {
+		dispatch({type: "GET_CASH", payload: cash})
+	}
+
+	return (
 		<div className="App">
 	  		<div className={'app'}>
-				<div style={{display:"flex"}}>
-		  			<button>
+			  <div style={{fontSize:"3rem"}}>{cash}</div>
+				<div style={{display:"flex",justifyContent:"center"}}>
+		  			<button style={{padding:"10px 30px", margin:"20px 20px"}} onClick={()=>addCash(Number(prompt()))}>
 						Пополнить счет
 		  			</button>
-		  			<button>
+		  			<button style={{padding:"10px 30px", margin:"20px 20px"}} onClick={()=>getCash(Number(prompt()))}>
 						Снять со счета
 		  			</button>
 				</div>
