97a2ec9d92d2b1ebaf6da5c7d69e80af1a02e8e2
Andrey Podolyako <andreypodolyako@gmail.com>
2021-03-07 11:46:53 +0300

    clean

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+		<div className="App">
+	  		<div className={'app'}>
+				<div style={{display:"flex"}}>
+		  			<button>
+						Пополнить счет
+		  			</button>
+		  			<button>
+						Снять со счета
+		  			</button>
+				</div>
+	  		</div>
+		</div>
+  	);
+}
+
+export default App;
